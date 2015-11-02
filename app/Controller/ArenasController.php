7428561a5a1687acd7bd65f5ced1074c393d6748
Marina <marina.hu@laposte.net>
2015-11-02 17:01:17 +0100

    Classes principales vierges

---
+++ app/Controller/ArenasController.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Main controller of the application
+ *
+ * @author Mali
+ */
+class ArenasController extends AppController {
+    
+    /**
+     * Index method : first page
+     *
+     * @return void
+     */
+    public function index() {
+
+    }
+
+    /**
+     * Login page
+     */
+    public function login() {
+        
+    }
+
+    /**
+     * Fighter page
+     */
+    public function fighter() {
+        
+    }
+
+    /**
+     * Sight page
+     */
+    public function sight() {
+        
+    }
+
+    /**
+     * Diary page
+     */
+    public function diary() {
+        
+    }
+    
+}
